af407be363e48a6716820646e9483a629ec65b56
CHETAN BAVISKAR <chetanbaviskar@pc-7-179.customer.ask4.lan>
2022-04-08 23:44:15 +0100

    adding DeploymentContract file

--- BF_CodeExample/DeployedContract.sol
+++ BF_CodeExample/DeployedContract.sol
@@ -144,7 +144,7 @@ abstract contract Context {
  * functions have been added to mitigate the well-known issues around setting
  * allowances. See {IERC20-approve}.
  */
-contract ERC20 is Context, IERC20, IERC20Metadata {
+contract X21166374ERC20 is Context, IERC20, IERC20Metadata {
     mapping(address => uint256) private _balances;
 
     mapping(address => mapping(address => uint256)) private _allowances;
